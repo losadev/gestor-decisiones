6edf2a131cb1ea372dbd607df703b44b5731b2f2
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-04-10 21:35:10 +0200

    Feature/registro usuario (#7)
    * Modelo/schema creado para User
    
    * Feature: creado el registro del usuario en el backend

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,12 +1,20 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
+import { User } from "../models/user.model";
 
-const sequalize = new Sequelize("proyecto-final-db", "postgres", "password", {
+export const sequelize = new Sequelize({
+  database: "proyecto-final-db",
+  username: "postgres",
+  password: "password",
   host: "localhost",
   dialect: "postgres",
-});
+  models: [User],
 
 export const connectionDB = async () => {
   try {
+ 
+    await sequelize.authenticate();
+    console.log("La conexión ha sido establecida");
+
     await sequalize.authenticate();
     console.log("La conexion ha sido establecida");
   } catch (error) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,19 @@
+import express from "express";
+import { connectionDB, sequelize } from "./config/db.config";
+import useRouter from "./routes/user.route";
+
+
 import express, { Request, Response } from "express";
 import { connectionDB } from "./config/db.config";
 const app = express();
+app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (request: Request, response: Response) => {
-  response.status(200).send("Hello World");
-});
+app.use("/api/register", useRouter);
 
 app.listen(PORT, async () => {
   connectionDB();
+  await sequelize.sync({ force: true });
   console.log("Server running at PORT ", PORT);
 });
