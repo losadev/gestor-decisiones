e8c26530526aff9d35931bc843e14ade30ccf240
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-04-11 18:51:06 +0200

    Feature: login y autenticación (#8)

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { User } from "../models/user.model";
+import bcrypt from "bcryptjs";
+import { generateAccessJWT } from "../services/generateAccessJWT";
+
+export const login = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  const existingUser = await User.findOne({ where: { email } });
+
+  if (!existingUser) {
+    res
+      .status(400)
+      .json({ message: "No existe ningun usuario con ese correo electrónico" });
+    return;
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    existingUser.password
+  );
+
+  if (!isPasswordValid) {
+    res.status(401).json({ message: "La contraseña es incorrecta" });
+    return;
+  }
+  const { password, ...userWithoutPassword } = existingUser.get({
+    plain: true,
+  });
+
+  let options = {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none" as const,
+  };
+  const token = generateAccessJWT({ id: existingUser.id });
+  res.cookie("access_token", token, options);
+
+  res.status(200).json({
+    message: "Usuario loegueado con éxito",
+    token,
+    data: userWithoutPassword,
+  });
+};
