3f2a221ae5258de9256e1e32bf53af4d896f6899
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-04-12 23:00:07 +0200

    Refactor: creadas variables para la configuración de la BD (#9)

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { User } from "../models/user.model";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  database: "proyecto-final-db",
-  username: "postgres",
-  password: "password",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PWD,
   host: "localhost",
   dialect: "postgres",
   models: [User],
