f6483318c321e2d181432e9527a94426405cc481
losadev <losadauresdev@gmail.com>
2025-04-19 11:42:19 +0200

    Feature: creación de una evlauación

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import { verifyUser } from "./middlewares/verifyUser";
 import cookieParser from "cookie-parser";
 import decisionRouter from "./routes/decision.routes";
+import evaluationRouter from "./routes/evaluation.router";
 
 const app = express();
 app.use(express.json());
@@ -16,11 +17,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/register", userRouter);
 app.use("/api/login", login);
-app.get("/api/try", verifyUser, (req: Request, res: Response) => {
-  console.log("Esto es req.user : ", req.user);
-  res.json({ data: req.body, message: "Funciona", token: req.cookies });
-});
+// app.get("/api/try", verifyUser, (req: Request, res: Response) => {
+//   console.log("Esto es req.user : ", req.user);
+//   res.json({ data: req.body, message: "Funciona", token: req.cookies });
+// });
 app.use("/api/decision", verifyUser, decisionRouter);
+app.use("/api/evaluation", verifyUser, evaluationRouter);
 
 app.get("/", (_req, res) => {
   res.send("Hello, world!");
