a48095a5ef07d7e95ffc6d226f479e117b174c84
losadev <losadauresdev@gmail.com>
2025-05-01 18:08:05 +0200

    Feature: creacion de la ruta Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import DecisionForm from './components/Decision/DecisionForm';
 import LoginForm from './components/Login/LoginForm';
 import FormRegister from './components/Register/FormRegister';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home/Home';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route index element={<Home />} />
                 <Route path="/register" element={<FormRegister />} />
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/create-decision" element={<DecisionForm />} />
