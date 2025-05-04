29df0d57b15a0e182a625bd7de4339598f23a660
losadev <losadauresdev@gmail.com>
2025-05-04 18:57:32 +0200

    Feature: el navbar del Home

--- frontend/src/components/Register/LoginLink.tsx
+++ frontend/src/components/Register/LoginLink.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router';
+
 function LoginLink() {
     return (
         <div className="flex justify-center gap-2 mt-4">
             <p className="text-gray-500">¿Ya tienes una cuenta?</p>
-            <a href="#" className="underline">
+            <Link to="/login" className="underline">
                 Iniciar sesión
-            </a>
+            </Link>
         </div>
     );
 }
