021990473e1980b9bcb0caf37136746d0f72b576
losadev <losadauresdev@gmail.com>
2025-05-04 21:19:15 +0200

    Feature: home cards

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,9 @@
+import { AnalyticsIcon } from '../../components/Icons/AnalyticsIcon';
+import { DecisionTrackingIcon } from '../../components/Icons/DecisionTracking';
+import Card from './Card';
 import Hero from './Hero';
 import NavBar from './NavBar';
+import { FcIdea } from 'react-icons/fc';
 
 const Home = () => {
     return (
@@ -7,6 +11,23 @@ const Home = () => {
             <NavBar />
             <main>
                 <Hero />
+                <section className="flex flex-col items-center bg-gray-50 py-16">
+                    <h1 className="text-5xl font-semibold">Características</h1>
+                    <div className="flex gap-16 px-8 justify-center mt-16">
+                        <Card
+                            text="Registro y seguimiento de decisiones"
+                            icon={<DecisionTrackingIcon />}
+                        />
+                        <Card
+                            text="Análisis visual (gráficos, puntuaciones)  "
+                            icon={<AnalyticsIcon />}
+                        />
+                        <Card
+                            text="Recomendaciones basadas en tus elecciones pasadas"
+                            icon={<FcIdea size={48} />}
+                        />
+                    </div>
+                </section>
             </main>
             {/*FOOTER*/}
         </>
