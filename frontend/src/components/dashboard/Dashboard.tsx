bea7029fc97ae2b7937c240b9cada50e7a32f66a
losadev <losadauresdev@gmail.com>
2025-05-05 21:26:00 +0200

    Feature: creado el NavBar para el dashboard

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,19 +1,23 @@
 import { Link } from 'react-router';
 import RecentActivity from './RecentActivity';
+import NavBar from './NavBar';
 
 const Dashboard = () => {
     return (
-        <main>
-            <header>
-                <h1>Gestor de decisiones personales</h1>
-                <Link to="/create-decision">Crear decision</Link>
-            </header>
-            <section>
-                <RecentActivity />
-                <div>quick stats</div>
-            </section>
-            <section>LISTA Y FILTRADO DE DECISIONES</section>
-        </main>
+        <div className="flex flex-row">
+            <NavBar />
+            <main>
+                <header>
+                    <h1>Gestor de decisiones personales</h1>
+                    <Link to="/create-decision">Crear decision</Link>
+                </header>
+                <section>
+                    <RecentActivity />
+                    <div>quick stats</div>
+                </section>
+                <section>LISTA Y FILTRADO DE DECISIONES</section>
+            </main>
+        </div>
     );
 };
 
