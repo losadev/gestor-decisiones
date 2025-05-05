7c41ceed0805bf787882f08d43809a83c2f96125
losadev <losadauresdev@gmail.com>
2025-05-05 22:34:02 +0200

    Feature: creada la card para estadisticas

---
+++ frontend/src/components/dashboard/QuickStatsCard.tsx
@@ -0,0 +1,31 @@
+const QuickStatsCard = () => {
+    return (
+        <div className="rounded-lg bg-white shadow-md p-4 inline-flex flex-col gap-1">
+            <div className="flex flex-col gap-2 p-4 border-b-amber-100">
+                <h1 className="text-3xl font-semibold">Estadísticas rápidas</h1>
+                <p className="text-gray-500">Tus últimas decisiones evaluadas / creadas</p>
+
+                <div className="grid grid-cols-2 grid-rows-[1fr 1fr] gap-4 mt-4 font-medium ">
+                    <div className="flex flex-col justify-between gap-4 border border-gray-300 shadow-md  rounded-lg py-6 px-4">
+                        <span>Total de decisiones</span>
+                        <span className="text-3xl font-bold">10</span>
+                    </div>
+                    <div className="flex flex-col justify-between gap-4 border border-gray-300 shadow-md rounded-lg py-6 px-4">
+                        <span>Decisiones pendientes</span>
+                        <span className="text-3xl font-bold">5</span>
+                    </div>
+                    <div className="flex flex-col justify-between gap-4 border border-gray-300 shadow-md rounded-lg p-8">
+                        <span>Buenas decisiones</span>
+                        <span className="text-3xl font-bold text-green-700">68%</span>
+                    </div>
+                    <div className="flex justify-between flex-col gap-4 border border-gray-300 shadow-md rounded-lg p-8">
+                        <span>Malas decisiones</span>
+                        <span className="text-3xl font-bold text-red-700">32%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuickStatsCard;
