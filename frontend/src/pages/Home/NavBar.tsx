a1468e3d6fc42c5f207a3028e812140d50c004be
losadev <losadauresdev@gmail.com>
2025-05-07 21:39:41 +0200

    Cambios en el NavBar para cuando el usuario esté logueado

--- frontend/src/pages/Home/NavBar.tsx
+++ frontend/src/pages/Home/NavBar.tsx
@@ -1,20 +1,40 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 const NavBar = () => {
+    const { user } = useAuth();
+
     return (
         <nav className="sm:flex border-b justify-between hidden items-center w-full bg-orange-500 border-b-gray-300 py-4 px-8 shadow-lg">
             <NavLink to="/">[LOGO]</NavLink>
             <div className="flex gap-8 items-center">
-                <NavLink
-                    to="/login"
-                    className="bg-orange-100 px-3 py-1 rounded font-medium hover:text-white">
-                    Iniciar sesión
-                </NavLink>
-                <NavLink
-                    to="/register"
-                    className="bg-black text-white px-3 py-1 rounded font-medium">
-                    Registrarme
-                </NavLink>
+                {user ? (
+                    <>
+                        <NavLink
+                            to="/dashboard"
+                            className="bg-black text-white px-3 py-1 rounded font-medium">
+                            Ir al dashboard
+                        </NavLink>
+                        <NavLink
+                            to="/logout"
+                            className="bg-orange-100 px-3 py-1 rounded font-medium hover:text-white">
+                            Cerrar sesión
+                        </NavLink>
+                    </>
+                ) : (
+                    <>
+                        <NavLink
+                            to="/login"
+                            className="bg-orange-100 px-3 py-1 rounded font-medium hover:text-white">
+                            Iniciar sesión
+                        </NavLink>
+                        <NavLink
+                            to="/register"
+                            className="bg-black text-white px-3 py-1 rounded font-medium">
+                            Registrarme
+                        </NavLink>
+                    </>
+                )}
             </div>
         </nav>
     );
