e96996e7fd9412df70297403c57f8d4282db1dc9
losadev <losadauresdev@gmail.com>
2025-05-12 17:40:06 +0200

    Styles: boton para ver detalle de una decision

--- frontend/src/components/Dashboard/RecentActivity.tsx
+++ frontend/src/components/Dashboard/RecentActivity.tsx
@@ -1,3 +1,6 @@
+import { FaArrowRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
 export const recentDecisions = [
     {
         id: '1',
@@ -32,6 +35,7 @@ export const recentDecisions = [
 ];
 
 const RecentActivity = () => {
+    const navigate = useNavigate();
     return (
         <div className="rounded-lg bg-white shadow-md py-8 px-4 inline-flex flex-col gap-1 flex-1 lg:px-8">
             <h1 className="text-3xl font-semibold">Actividad reciente</h1>
@@ -41,19 +45,30 @@ const RecentActivity = () => {
                     <div
                         key={decision.id}
                         className="flex flex-col gap-2 py-4 px-1 border-b border-b-gray-300">
-                        <h3 className="font-semibold text-lg">{decision.title}</h3>
-                        <div className="flex gap-2">
-                            <span className="rounded-xl border text-blue-200 bg-blue-800 px-2">
-                                {decision.category}
-                            </span>
-                            <span
-                                className={`rounded-xl border ${
-                                    decision.status === 'evaluated'
-                                        ? 'text-green-200 bg-green-800'
-                                        : 'text-orange-500'
-                                } px-2`}>
-                                {decision.status === 'evaluated' ? 'Evaluado' : 'En progreso'}
-                            </span>
+                        <div className="flex gap-2 items-center">
+                            <div className="grow gap-2">
+                                <h3 className="font-semibold text-lg">{decision.title}</h3>
+                                <div className="flex gap-2">
+                                    <span className="rounded-xl border text-blue-200 bg-blue-800 px-2">
+                                        {decision.category}
+                                    </span>
+                                    <span
+                                        className={`rounded-xl border ${
+                                            decision.status === 'evaluated'
+                                                ? 'text-green-200 bg-green-800'
+                                                : 'text-orange-500'
+                                        } px-2`}>
+                                        {decision.status === 'evaluated'
+                                            ? 'Evaluado'
+                                            : 'En progreso'}
+                                    </span>
+                                </div>
+                            </div>
+                            <button
+                                onClick={() => navigate(`/decision/${decision.id}`)}
+                                className="p-2 hover:bg-gray-100 rounded hover:cursor-pointer">
+                                <FaArrowRight className="text-gray-500 text-xl" />
+                            </button>
                         </div>
                     </div>
                 ))}
