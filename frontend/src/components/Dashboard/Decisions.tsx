062af3c8d2b4cc46e1ead3749a1c5307e43e147a
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-05-23 15:34:48 +0200

    Feature: pagina decision respnsive (#71)

--- frontend/src/components/Dashboard/Decisions.tsx
+++ frontend/src/components/Dashboard/Decisions.tsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from 'react';
 import DecisionsTable from './DecisionsTable';
 import NewDecisionButton from './NewDecisionButton';
+import DecisionForm from '../Decision/DecisionForm';
 
 const Decisions = () => {
+    const [modal, setModal] = useState<boolean>(false);
+
+    const openModal = () => {
+        setModal(true);
+    };
+
+    const closeModal = () => {
+        setModal(false);
+    };
+
+    useEffect(() => {
+        const main = document.getElementById('main-scroll');
+        if (main) {
+            main.style.overflow = modal ? 'hidden' : 'auto';
+        }
+    }, [modal]);
     return (
-        <div className="flex flex-col w-full px-8 py-8 gap-4">
-            <header className="flex flex-col justify-between items-center px-4 py-4 gap-8 sm:flex sm:flex-row sm:items-center md:flex md:flex-row :items-center">
+        <div className="flex flex-col gap-4 p-4 w-full mx-auto">
+            <header className="flex flex-col justify-between items-center px-0 py-4 gap-8 sm:flex sm:flex-row sm:items-center md:flex md:flex-row :items-center">
                 <h1 className="text-2xl font-bold text-center sm:text-3xl">
                     Gestor de decisiones personales
                 </h1>
-                <NewDecisionButton />
+                <NewDecisionButton onClick={openModal} />
             </header>
-            <main className="h-full ">
+            <main className="flex-1 w-full">
                 <DecisionsTable />
             </main>
+            <DecisionForm isOpen={modal} onClose={closeModal} />
         </div>
     );
 };
--- frontend/src/components/Dashboard/DecisionsTable.tsx
+++ frontend/src/components/Dashboard/DecisionsTable.tsx
@@ -31,7 +31,6 @@ function DecisionsTable() {
             const distanceToTop = actionRect.top - tableRect.top;
             const dropdownHeight = 150;
 
-            // Si el dropdown no cabe abajo, abre hacia arriba
             const shouldOpenUpward =
                 distanceToBottom < dropdownHeight && distanceToTop > dropdownHeight;
             setOpenUpwardIndex(shouldOpenUpward ? index : null);
@@ -40,9 +39,7 @@ function DecisionsTable() {
 
     useEffect(() => {
         axios
-            .get('http://localhost:5000/api/decision', {
-                withCredentials: true,
-            })
+            .get('http://localhost:5000/api/decision', { withCredentials: true })
             .then((response) => {
                 setData(response.data.decisions);
             })
@@ -52,14 +49,12 @@ function DecisionsTable() {
     useEffect(() => {
         let filtered = [...data];
 
-        // Filtrar por texto
         if (searchDecision.trim() !== '') {
             filtered = filtered.filter((item) =>
                 item.title.toLowerCase().includes(searchDecision.toLowerCase())
             );
         }
 
-        // Filtrar por estado
         if (active === 2) {
             filtered = filtered.filter((d) => d.status === 'progress');
         } else if (active === 3) {
@@ -70,80 +65,84 @@ function DecisionsTable() {
     }, [searchDecision, active, data]);
 
     const deleteDecision = (id: string) => {
-        const res = axios.delete(`http://localhost:5000/api/decision/${id}`, {
-            withCredentials: true,
-        });
-        res.then((response) => {
-            setMessage(response.data.message);
-        }).catch((error) => {
-            setMessage(error);
-        });
+        axios
+            .delete(`http://localhost:5000/api/decision/${id}`, {
+                withCredentials: true,
+            })
+            .then((response) => {
+                setMessage(response.data.message);
+            })
+            .catch((error) => {
+                setMessage(error.message);
+            });
     };
 
     return (
-        <section className="px-4 pb-8 h-full ">
-            <div className="bg-white shadow-md rounded-lg p-6 lg:px-8 w-full h-full">
-                <h1 className="text-3xl font-semibold">Tus decisiones</h1>
+        <section className="w-full h-full min-h-20 0">
+            <div className="bg-white shadow-md rounded-lg p-4 sm:p-6 lg:px-8 w-full min-h-20 h-full">
+                <h2 className="text-2xl font-semibold">Tus decisiones</h2>
                 <p className="mt-1 text-gray-600">
-                    Gestiona y haz seguimiento todas tus decisiones
+                    Gestiona y haz seguimiento de todas tus decisiones
                 </p>
+
                 <div className="my-4">
                     <input
                         type="search"
                         placeholder="Busca tu decisión aquí..."
-                        className="border border-gray-300 rounded-lg px-4 py-1"
+                        className="border border-gray-300 rounded-lg px-4 py-2 w-full sm:w-96"
                         value={searchDecision}
                         onChange={(e: ChangeEvent<HTMLInputElement>) =>
                             setSearchDecision(e.target.value)
                         }
                     />
                 </div>
-                {/*<DecisionFilters active={} setActive={}/>*/}
-                <div className="inline-flex bg-gray-100 my-4 p-2 text-sm sm:text-lg items-center justify- rounded-lg">
-                    <ul className="flex flex-row font-medium items-center text-gray-800">
+
+                <div className="inline-flex bg-gray-100 my-4 p-2 text-sm sm:text-base items-center rounded-lg">
+                    <ul className="flex flex-row font-medium items-center text-gray-800 gap-2 sm:gap-4">
                         <li
-                            className={`${active === 1 ? 'bg-orange-500' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer  transition`}
+                            className={`${active === 1 ? 'bg-orange-500 text-white' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer transition`}
                             onClick={() => setActive(1)}>
                             Todas
                         </li>
                         <li
-                            className={`${active === 2 ? 'bg-orange-500' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer  transition`}
+                            className={`${active === 2 ? 'bg-orange-500 text-white' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer transition`}
                             onClick={() => setActive(2)}>
                             Pendientes
                         </li>
                         <li
-                            className={`${active === 3 ? 'bg-orange-500' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer  transition`}
+                            className={`${active === 3 ? 'bg-orange-500 text-white' : 'hover:bg-gray-200'} px-3 py-1 rounded cursor-pointer transition`}
                             onClick={() => setActive(3)}>
                             Evaluadas
                         </li>
                     </ul>
                 </div>
-                <div className="rounded-lg  w-full overflow-auto shadow-sm">
-                    <table className="w-full min-h-20 custom-table text-xl" ref={tableRef}>
-                        <thead className=" text-white h-12">
+
+                <div className="rounded-lg w-full overflow-x-auto shadow-sm !min-h-20">
+                    <table className="min-w-max w-full text-sm sm:text-base " ref={tableRef}>
+                        <thead className="bg-orange-500 text-black h-12">
                             <tr className="text-left">
-                                <th className="px-8 py-2">Título</th>
-                                <th className="px-8 py-2">Categoría</th>
-                                <th className="px-8 py-2">Fecha</th>
-                                <th className="px-8 py-2">Estado</th>
-                                <th className="px-8 py-2">Acciones</th>
+                                <th className="px-4 sm:px-6 py-3">Título</th>
+                                <th className="px-4 sm:px-6 py-3">Categoría</th>
+                                <th className="px-4 sm:px-6 py-3">Fecha</th>
+                                <th className="px-4 sm:px-6 py-3">Estado</th>
+                                <th className="px-4 sm:px-6 py-3">Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {searchFilterItems?.map((decision, index) => (
-                                <tr key={index} className="hover:bg-gray-200 transition">
-                                    <td className="px-8 py-2">{decision.title}</td>
-                                    <td className="px-8 py-2">{decision.category}</td>
-                                    <td className="px-8 py-2">
+                            {searchFilterItems.map((decision, index) => (
+                                <tr key={index} className="hover:bg-gray-100 transition">
+                                    <td className="px-4 sm:px-6 py-3">{decision.title}</td>
+                                    <td className="px-4 sm:px-6 py-3">{decision.category}</td>
+                                    <td className="px-4 sm:px-6 py-3">
                                         {decision.createdAt.split('T')[0]}
                                     </td>
-                                    <td className="px-8 py-2">
+                                    <td className="px-4 sm:px-6 py-3">
                                         <Chip mode={decision.status} />
                                     </td>
-                                    <td className="px-8 py-2 relative">
+                                    <td className="px-4 sm:px-6 py-3 relative">
                                         <button
                                             type="button"
-                                            className="cursor-pointer hover:bg-orange-200 rounded-lg p-3"
+                                            className="cursor-pointer hover:bg-orange-200 rounded-lg p-2"
                                             onClick={() => handleActions(index)}>
                                             <IoEllipsisHorizontalOutline />
                                         </button>
