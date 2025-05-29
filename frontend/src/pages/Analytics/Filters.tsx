e7b7d79582b3ee1ef10b234a337dd25423c7530c
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-05-29 18:00:08 +0200

    Feature: filtros para móviles (#92)

--- frontend/src/pages/Analytics/Filters.tsx
+++ frontend/src/pages/Analytics/Filters.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { LiaFilterSolid } from 'react-icons/lia';
 import { CategoryType } from '../../types/decision.types';
 import { TbWashDrycleanOff } from 'react-icons/tb';
@@ -17,15 +18,63 @@ const Filters = ({
     setSelectedTimeRange,
 }: FiltersProps) => {
     const categories: string[] = ['Categorías', ...Object.values(CategoryType)];
+    const [showMobileFilters, setShowMobileFilters] = useState(false);
+
+    const resetFilters = () => {
+        setSelectedCategory('Todas las categorías');
+        setSelectedTimeRange('Todo');
+        setShowMobileFilters(false);
+    };
 
     return (
-        <div className="flex items-center mt-8 pb-2 gap-2 justify-end md:justify-between">
-            <button className="border flex gap-2 items-center bg-white border-gray-300 px-4 py-2 rounded md:hidden">
-                <LiaFilterSolid />
-                <span className="font-medium">Filtros: </span>
-            </button>
+        <div className="mt-8 pb-2">
+            {/* para abrir el panel en mobile */}
+            <div className="flex items-center justify-end md:justify-between gap-2 md:hidden">
+                <button
+                    onClick={() => setShowMobileFilters(!showMobileFilters)}
+                    className="border flex gap-2 items-center bg-white border-gray-300 px-4 py-2 rounded font-medium hover:bg-gray-100 transition">
+                    <LiaFilterSolid />
+                    <span>Filtros</span>
+                </button>
+            </div>
 
-            <div className="hidden md:flex">
+            {/* para pantallas pequeñas */}
+            {showMobileFilters && (
+                <div className="flex flex-col gap-4 mt-4 md:hidden">
+                    <select
+                        value={selectedCategory}
+                        onChange={(e) => setSelectedCategory(e.target.value)}
+                        className="border border-gray-300 bg-white px-2 py-2 rounded font-medium cursor-pointer hover:bg-gray-100 transition">
+                        {categories.map((c) => (
+                            <option key={c} value={c}>
+                                {c}
+                            </option>
+                        ))}
+                    </select>
+
+                    <select
+                        value={selectedTimeRange}
+                        onChange={(e) => setSelectedTimeRange(e.target.value)}
+                        className="border bg-white border-gray-300 px-2 py-2 rounded cursor-pointer font-medium hover:bg-gray-100 transition">
+                        <option value="Todo">Fechas</option>
+                        <option value="30">Últimos 30 días</option>
+                        <option value="90">Últimos 3 meses</option>
+                        <option value="180">Últimos 6 meses</option>
+                        <option value="365">Último año</option>
+                    </select>
+
+                    <button
+                        type="button"
+                        onClick={resetFilters}
+                        className="border bg-white border-gray-300 px-4 py-2 rounded flex items-center gap-2 cursor-pointer font-medium hover:bg-gray-100 transition hover:shadow-sm">
+                        <TbWashDrycleanOff />
+                        <span>Limpiar filtros</span>
+                    </button>
+                </div>
+            )}
+
+            {/*para pantallas grandes */}
+            <div className="hidden md:flex items-center gap-2">
                 <div className="flex gap-2 items-center">
                     <LiaFilterSolid size={20} />
                     <span className="font-medium">Filtros: </span>
@@ -45,26 +94,23 @@ const Filters = ({
                     <select
                         value={selectedTimeRange}
                         onChange={(e) => setSelectedTimeRange(e.target.value)}
-                        className="border bg-white border-gray-300 px-2 py-1 rounded w-40 cursor-pointer font-medium hover:bg-gray-100 transition">
+                        className="border bg-white border-gray-300 px-2 py-2 rounded w-40 cursor-pointer font-medium hover:bg-gray-100 transition">
                         <option value="Todo">Fechas</option>
                         <option value="30">Últimos 30 días</option>
                         <option value="90">Últimos 3 meses</option>
                         <option value="180">Últimos 6 meses</option>
                         <option value="365">Último año</option>
                     </select>
+
+                    <button
+                        type="button"
+                        onClick={resetFilters}
+                        className="border bg-orange-100 border-orange-300 hover:bg-orange-200 px-4 py-2 rounded flex items-center gap-2 cursor-pointer font-medium  transition hover:shadow-sm">
+                        <TbWashDrycleanOff />
+                        <span>Limpiar filtros</span>
+                    </button>
                 </div>
             </div>
-
-            <button
-                type="button"
-                onClick={() => {
-                    setSelectedCategory('Todas las categorías');
-                    setSelectedTimeRange('Todo');
-                }}
-                className="border bg-white border-gray-300 px-4 py-2 rounded flex items-center gap-2 cursor-pointer font-medium hover:bg-gray-100 transition hover:shadow-sm">
-                <TbWashDrycleanOff className="font-medium" />
-                <span>Limpiar filtros</span>
-            </button>
         </div>
     );
 };
