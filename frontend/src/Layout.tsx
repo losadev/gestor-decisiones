47f9ace5315b07463a20142b666f072e30211247
losadev <losadauresdev@gmail.com>
2025-05-30 15:45:52 +0200

    Creción del estado global con zustand

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,8 +1,12 @@
 import NavBar from './components/Dashboard/NavBar';
 import SideNavBar from './components/Dashboard/SideNavBar';
 import { Outlet } from 'react-router-dom';
+import { useSnackbarStore } from './store/snackbarStore';
+import Snackbar from './components/SnackBar';
 
 const Layout = () => {
+    const { open, message, closeSnackbar } = useSnackbarStore();
+
     return (
         <div className="flex flex-col 2xl:flex-row font-inter h-full ">
             <NavBar />
@@ -12,6 +16,7 @@ const Layout = () => {
                 className="flex flex-1 min-h-screen w-full overflow-y-auto scroll-custom bg-gray-100">
                 <Outlet />
             </main>
+            <Snackbar open={open} message={message} onClose={closeSnackbar} />
         </div>
     );
 };
