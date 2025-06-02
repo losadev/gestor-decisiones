d3a0bc2351bd41a395cdfd099653c980eea97487
losadev <losadauresdev@gmail.com>
2025-06-02 19:12:17 +0200

    Feature: Funcion controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -23,3 +23,37 @@ export const createUser = async (req: Request, res: Response) => {
     }
   }
 };
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const avatarUrl = req.file ? req.file.filename : undefined;
+
+    const updatedData = {
+      ...req.body,
+      ...(avatarUrl && { avatar: avatarUrl }),
+    };
+
+    const updatedUser = await userService.updateUser(userId, updatedData);
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "Usuario actualizado con éxito",
+      data: updatedUser,
+      success: true,
+    });
+  } catch (error: any) {
+    if (!res.headersSent) {
+      res.status(500).json({
+        message: "Ha ocurrido un error en el servidor",
+        success: false,
+      });
+    }
+  }
+};
