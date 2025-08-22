490291732980418961e84288cae6dd412f6a448f
Pablo Losada <160409337+losadev@users.noreply.github.com>
2025-08-22 21:58:29 +0200

    Add login request validator and test (#149)

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Router } from "express";
 import { login } from "../controllers/auth.controller";
 import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validateRequest";
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.post(
     .withMessage("Enter a valid email address")
     .normalizeEmail(),
   body("password").not().isEmpty(),
+  validateRequest,
   login
 );
 
